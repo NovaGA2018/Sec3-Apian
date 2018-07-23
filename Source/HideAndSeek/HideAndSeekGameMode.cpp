b394ae6ae4af8d3e6108fe10fdd1b306f669a3f0
Mike Howlett <mah9882@rit.edu>
2018-07-23 15:34:29 -0400

    First commit; Added Unreal project to source control.

---
+++ Source/HideAndSeek/HideAndSeekGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "HideAndSeekGameMode.h"
+#include "HideAndSeekCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AHideAndSeekGameMode::AHideAndSeekGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
